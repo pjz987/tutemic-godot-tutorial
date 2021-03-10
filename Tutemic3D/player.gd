f0ece18de394794624b40637ae3c343d1af269dd
Pete Jones <56445854+pjz987@users.noreply.github.com>
2021-03-10 06:14:06 -0800

    sesh 2

--- Tutemic3D/player.gd
+++ Tutemic3D/player.gd
@@ -7,8 +7,24 @@ export var _mouse_sensitivity := 0.05
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+
 func _input(event) -> void:
+	aim(event)
+
+
+func _physics_process(delta) -> void:
+	if Input.is_action_pressed("move_forward"):
+		print("moving forward")
+
+
+func aim(event: InputEvent) -> void:
 	var mouse_motion = event as InputEventMouseMotion
 	if mouse_motion:
 		rotation_degrees.y -= mouse_motion.relative.x * _mouse_sensitivity
-		$Camera.rotation_degrees.x -= mouse_motion.relative.y * _mouse_sensitivity
+		
+		var current_tilt: float = $Camera.rotation_degrees.x
+		current_tilt -= mouse_motion.relative.y * _mouse_sensitivity
+		
+		$Camera.rotation_degrees.x = clamp(current_tilt, -90, 90)
+
+	
