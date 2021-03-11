f71ae30100be7996b99ce87c0e18ee4346e965d0
Pete Jones <56445854+pjz987@users.noreply.github.com>
2021-03-10 16:28:16 -0800

    third sesh

--- Tutemic3D/player.gd
+++ Tutemic3D/player.gd
@@ -1,9 +1,8 @@
 extends KinematicBody
 
-export var _mouse_sensitivity := 0.05
+export var _mouse_sensitivity := 0.08
+export var _move_speed: float = 3
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
@@ -13,8 +12,7 @@ func _input(event) -> void:
 
 
 func _physics_process(delta) -> void:
-	if Input.is_action_pressed("move_forward"):
-		print("moving forward")
+	movement()
 
 
 func aim(event: InputEvent) -> void:
@@ -27,4 +25,23 @@ func aim(event: InputEvent) -> void:
 		
 		$Camera.rotation_degrees.x = clamp(current_tilt, -90, 90)
 
+
+func movement() -> void:
+	var movement_vector: Vector3
+	var forward_movement: Vector3
+	var sideways_movement: Vector3
 	
+	if Input.is_action_pressed("move_forward"):
+		forward_movement = -transform.basis.z
+	elif Input.is_action_pressed("move_backward"):
+		forward_movement = transform.basis.z
+
+	if Input.is_action_pressed("move_left"):
+		sideways_movement = -transform.basis.x
+	elif Input.is_action_pressed("move_right"):
+		sideways_movement = transform.basis.x
+	
+	movement_vector = forward_movement + sideways_movement
+	movement_vector = movement_vector.normalized()
+	
+	move_and_slide(movement_vector * _move_speed)
