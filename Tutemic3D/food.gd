536c753b31b9e4cdc8062cafedd162b6f5359835
Pete Jones <56445854+pjz987@users.noreply.github.com>
2021-03-14 17:24:10 -0700

    seventh sesh

--- Tutemic3D/food.gd
+++ Tutemic3D/food.gd
@@ -3,8 +3,13 @@ extends Area
 class_name Food
 
 func _ready():
-	GameEvents.connect("food_clicked", self, "_on_mouse_entered")
+	GameEvents.connect("food_moused_over", self, "_on_mouse_entered")
+	GameEvents.connect("food_moused_out", self, "_on_mouse_out")
 
 
-func _on_mouse_entered():
-	print("mouse over food")
+func _on_mouse_entered(food: Food):
+	if food == self:
+		$SpotLight.visible = true
+
+func _on_mouse_out():
+	$SpotLight.visible = false
--- Tutemic3D/food_chooser_ray.gd
+++ Tutemic3D/food_chooser_ray.gd
@@ -1,8 +1,14 @@
 extends RayCast
 
+var _is_mousing_over := false
 
 func _input(event):
 	var collided_area: Area = get_collider()
+	
 	if collided_area is Food:
-		print(collided_area.name)
-		GameEvents.emit_signal("food_moused_over", collided_area)
+		if !_is_mousing_over:
+			_is_mousing_over = true
+			GameEvents.emit_signal("food_moused_over", collided_area)
+	elif _is_mousing_over:
+		_is_mousing_over = false
+		GameEvents.emit_signal("food_moused_out")
